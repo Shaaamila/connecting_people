c258e07a10345ac2522801a5a2250958f84b3d0b
Shaaamila <shamila.m09@gmail.com>
2022-11-23 11:55:32 -0800

    first commit

---
+++ connecting_people_backend/schemas/schema.js
@@ -0,0 +1,17 @@
+// schema creator
+import createSchema from 'part:@sanity/base/schema-creator'
+
+// schema types from any plugins that might expose them
+import schemaTypes from 'all:part:@sanity/base/schema-type'
+import user from './user';
+
+// give schema to the builder and provide the result to Sanity
+export default createSchema({
+  // name schema
+  name: 'default',
+  // Then proceed to concatenate our document type
+  // to the ones provided by any plugins that are installed
+  types: schemaTypes.concat([
+    user
+  ]),
+})
